ad6ee9bdd90a970113c7a0f6b6954518f5ffe094
王瀚 <5211470+wanghanwanghan@user.noreply.gitee.com>
2024-03-20 10:19:32 +0800

    fix: conflict

--- result.php
+++ result.php
@@ -4,6 +4,7 @@
 #$inc_tieValue //
 #$inc_urlPath  //صַ
 */
+// XueSi test 1231232131231232132321323
 //1231232131 XueSi test
 $resultBuffer=implode("",file($template_result_path));
 $resultBuffer=str_replace("{title}",$inc_title,$resultBuffer);
